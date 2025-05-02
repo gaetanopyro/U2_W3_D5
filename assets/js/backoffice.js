faf8602a65ef158b9696916db6e99a0d4b134c26
gaetanopyro <gaetano.miscio@gmail.com>
2025-05-02 20:55:21 +0200

    index

--- assets/js/backoffice.js
+++ assets/js/backoffice.js
@@ -1,7 +1,6 @@
 const URL = "https://striveschool-api.herokuapp.com/api/product";
 const form = document.getElementById("form_enter");
 const deleteBtn = document.getElementById("delete-btn");
-console.log(form);
 
 form.onsubmit = function (e) {
   e.preventDefault();
@@ -28,25 +27,16 @@ form.onsubmit = function (e) {
       Authorization:
         "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2ODE0OGI5MzFjMjUwNDAwMTUxYWI2OTIiLCJpYXQiOjE3NDYxNzY5MTUsImV4cCI6MTc0NzM4NjUxNX0.muFLEb0JxkjnCUW9S0O3BcEh5hekLALr1glNBR0sIn0",
     },
-  })
-    .then((Response) => {
-      console.log(Response);
-      if (!Response.ok) {
-        throw new Error("Errore nella fetch");
-      }
-      return Response.json();
-    })
-    .catch((error) => console.log(error));
+  });
 
   console.log("SUBMIT", newInformation);
 };
 
 deleteBtn.onclick = function () {
   fetch(URL, { method: "DELETE" })
-    .then((response) => {
-      if (response.ok) {
+    .then((resp) => {
+      if (resp.ok) {
         alert("Item eliminato");
-
         window.location.assign("./index.html");
       }
     })
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,30 @@
 const URL = "https://striveschool-api.herokuapp.com/api/product";
+const row = document.getElementById("newRow");
+
+const cardItem = () => {
+  fetch(URL, {
+    headers: {
+      Authorization: "3nJnQe6flj3plvWrzpIZAgDGxldj59FeS5wCV7abjJcDQpVi4N5RgDMl",
+    },
+  })
+    .then((response) => {
+      console.log(response);
+      if (!response.ok) {
+        throw new Error("Problemi di rete?!");
+      }
+      return response.json();
+    })
+    .then((objItem) => {
+      row.innerHTML = "";
+
+      const row = document.querySelector(".newRow");
+       charactersObj.data.forEach(char => {
+         console.log(char.name);
+
+
+      objData.photos.forEach((imageUrl) => {
+        console.log(imageUrl);
+
+ window.onload = () => {
+  cardItem();
+};
